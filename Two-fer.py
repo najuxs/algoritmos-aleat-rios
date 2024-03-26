452ee2a5d3ebe94b3b4496286d17180a19cd1d4b
najuxs <comdoisnanninha@gmail.com>
2024-03-26 10:36:57 -0300

    Arquivos base

---
+++ Two-fer.py
@@ -0,0 +1,14 @@
+print("2-fer")
+
+def Grande ():
+    nome = input("Informe seu nome")
+
+    if not nome:
+        nome = ("você")
+    else:
+        pass
+
+    print(f"um para {nome}, um para mim")
+
+Grande()
+
